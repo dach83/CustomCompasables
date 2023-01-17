458514c6a1f3d1e87db37ccf50bffe8de55dfd73
dach83 <dach83@mail.ru>
2023-01-17 14:05:23 +0300

    Setup packages structure

---
+++ app/src/main/java/com/github/dach83/customcomposables/core/MainScreen.kt
@@ -0,0 +1,9 @@
+package com.github.dach83.customcomposables.core
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainScreen() {
+    Text(text = "MainScreen")
+}
