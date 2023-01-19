af49fb72a445b54a5bd45ac1afeef0e377d2e282
dach83 <dach83@mail.ru>
2023-01-19 11:08:35 +0300

    Expandable card

--- app/src/main/java/com/github/dach83/customcomposables/core/MainScreen.kt
+++ app/src/main/java/com/github/dach83/customcomposables/core/MainScreen.kt
@@ -1,9 +1,9 @@
 package com.github.dach83.customcomposables.core
 
 import androidx.compose.runtime.Composable
-import com.github.dach83.customcomposables.components.otpinputfield.OtpTextFieldScreen
+import com.github.dach83.customcomposables.components.expandablecard.ExpandableCardScreen
 
 @Composable
 fun MainScreen() {
-    OtpTextFieldScreen()
+    ExpandableCardScreen()
 }
